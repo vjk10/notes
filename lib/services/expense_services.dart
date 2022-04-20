1abeb303b200a5e85a88a7d156a8aac1ac46e6e1
Vishnu Jayakumar <vjk10barca@gmail.com>
2022-04-20 19:47:46 +0530

    :bug: Fix for Issue #25

--- lib/services/expense_services.dart
+++ lib/services/expense_services.dart
@@ -36,113 +36,132 @@ class ExpenseServices {
   }
 
   downloadExcel(String sheetName, List<ExpenseModel> expenses) async {
-    final excel = Excel.createExcel();
-    final sheet = excel[excel.getDefaultSheet()!];
-    if (kDebugMode) {
-      print("Default Sheet: " + sheet.toString());
-    }
-    // Adding Header
-    for (var header = 0; header < headers.length; header++) {
-      sheet
-          .cell(CellIndex.indexByColumnRow(columnIndex: header, rowIndex: 0))
-          .value = headers[header];
-    }
-    // Adding Records
-    for (var expense = 0; expense < expenses.length; expense++) {
-      sheet
-          .cell(
-              CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: expense + 1))
-          .value = expenses[expense].index;
-      sheet
-          .cell(
-              CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: expense + 1))
-          .value = expenses[expense].type;
-      sheet
-          .cell(
-              CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: expense + 1))
-          .value = expenses[expense].amount;
-      sheet
-          .cell(
-              CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: expense + 1))
-          .value = expenses[expense].description;
-    }
-    var fileBytes = excel.save();
-    try {
-      var file = File(join("$excelFilePath/$sheetName.xlsx"))
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(fileBytes!);
-      if (file.path.isNotEmpty) {
+    if (sheetName.isNotEmpty) {
+      final excel = Excel.createExcel();
+      final sheet = excel[excel.getDefaultSheet()!];
+      if (kDebugMode) {
+        print("Default Sheet: " + sheet.toString());
+      }
+      // Adding Header
+      for (var header = 0; header < headers.length; header++) {
+        sheet
+            .cell(CellIndex.indexByColumnRow(columnIndex: header, rowIndex: 0))
+            .value = headers[header];
+      }
+      // Adding Records
+      for (var expense = 0; expense < expenses.length; expense++) {
+        sheet
+            .cell(CellIndex.indexByColumnRow(
+                columnIndex: 0, rowIndex: expense + 1))
+            .value = expenses[expense].index;
+        sheet
+            .cell(CellIndex.indexByColumnRow(
+                columnIndex: 1, rowIndex: expense + 1))
+            .value = expenses[expense].type;
+        sheet
+            .cell(CellIndex.indexByColumnRow(
+                columnIndex: 2, rowIndex: expense + 1))
+            .value = expenses[expense].amount;
+        sheet
+            .cell(CellIndex.indexByColumnRow(
+                columnIndex: 3, rowIndex: expense + 1))
+            .value = expenses[expense].description;
+      }
+      var fileBytes = excel.save();
+      try {
+        var file = File(join("$excelFilePath/$sheetName.xlsx"))
+          ..createSync(recursive: true)
+          ..writeAsBytesSync(fileBytes!);
+        if (file.path.isNotEmpty) {
+          Get.showSnackbar(GetSnackBar(
+            shouldIconPulse: false,
+            backgroundColor: c.surface,
+            margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            borderRadius: 10,
+            icon: Icon(
+              Icons.file_download_done_outlined,
+              color: c.primary,
+            ),
+            duration: const Duration(seconds: 2),
+            titleText: Text(
+              "$sheetName downloaded successfully",
+              style: t.textTheme.caption?.copyWith(color: c.onSurface),
+            ),
+            messageText: Text(
+              "$excelFilePath/$sheetName.xlsx",
+              style: t.textTheme.bodySmall,
+            ),
+            mainButton: TextButton(
+              child: const Text("Open File"),
+              onPressed: () async {
+                if (kDebugMode) {
+                  print("$excelFilePath/$sheetName.xlsx");
+                }
+                try {
+                  await OpenDocument.openDocument(
+                      filePath: "$excelFilePath/$sheetName.xlsx");
+                } on OpenDocumentException catch (e) {
+                  Get.showSnackbar(GetSnackBar(
+                    shouldIconPulse: false,
+                    backgroundColor: c.surface,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 16),
+                    borderRadius: 10,
+                    icon: Icon(
+                      Icons.error,
+                      color: c.error,
+                    ),
+                    duration: const Duration(seconds: 2),
+                    messageText: Text(
+                      e.errorMessage,
+                      style: t.textTheme.caption?.copyWith(color: c.onSurface),
+                    ),
+                  ));
+                }
+              },
+            ),
+          ));
+        } else {
+          Get.showSnackbar(GetSnackBar(
+            shouldIconPulse: false,
+            backgroundColor: c.surface,
+            margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            borderRadius: 10,
+            icon: Icon(
+              Icons.file_download_off_outlined,
+              color: c.error,
+            ),
+            duration: const Duration(seconds: 2),
+            messageText: Text(
+              "Error downloading $sheetName",
+              style: t.textTheme.caption?.copyWith(color: c.onSurface),
+            ),
+          ));
+        }
+      } on FileSystemException catch (e) {
         Get.showSnackbar(GetSnackBar(
           shouldIconPulse: false,
           backgroundColor: c.surface,
           margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
           borderRadius: 10,
           icon: Icon(
-            Icons.file_download_done_outlined,
-            color: c.primary,
-          ),
-          duration: const Duration(seconds: 2),
-          titleText: Text(
-            "$sheetName downloaded successfully",
-            style: t.textTheme.caption?.copyWith(color: c.onSurface),
-          ),
-          messageText: Text(
-            "$excelFilePath/$sheetName.xlsx",
-            style: t.textTheme.bodySmall,
-          ),
-          mainButton: TextButton(
-            child: const Text("Open File"),
-            onPressed: () async {
-              if (kDebugMode) {
-                print("$excelFilePath/$sheetName.xlsx");
-              }
-              // OpenFilex.open(
-              //   "$excelFilePath/$sheetName.xlsx",
-              //   type:
-              //       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-              // );
-              try {
-                await OpenDocument.openDocument(
-                    filePath: "$excelFilePath/$sheetName.xlsx");
-              } on OpenDocumentException catch (e) {
-                Get.showSnackbar(GetSnackBar(
-                  shouldIconPulse: false,
-                  backgroundColor: c.surface,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-                  borderRadius: 10,
-                  icon: Icon(
-                    Icons.error,
-                    color: c.error,
-                  ),
-                  duration: const Duration(seconds: 2),
-                  messageText: Text(
-                    e.errorMessage,
-                    style: t.textTheme.caption?.copyWith(color: c.onSurface),
-                  ),
-                ));
-              }
-            },
-          ),
-        ));
-      } else {
-        Get.showSnackbar(GetSnackBar(
-          shouldIconPulse: false,
-          backgroundColor: c.surface,
-          margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-          borderRadius: 10,
-          icon: Icon(
-            Icons.file_download_off_outlined,
+            Icons.error,
             color: c.error,
           ),
           duration: const Duration(seconds: 2),
           messageText: Text(
-            "Error downloading $sheetName",
+            e.message,
             style: t.textTheme.caption?.copyWith(color: c.onSurface),
+          ),
+          mainButton: TextButton(
+            child: const Text("Open Settings"),
+            onPressed: () {
+              openAppSettings();
+            },
           ),
         ));
       }
-    } on FileSystemException catch (e) {
+    } else {
       Get.showSnackbar(GetSnackBar(
         shouldIconPulse: false,
         backgroundColor: c.surface,
@@ -154,17 +173,10 @@ class ExpenseServices {
         ),
         duration: const Duration(seconds: 2),
         messageText: Text(
-          e.message,
+          "Sheetname is empty!",
           style: t.textTheme.caption?.copyWith(color: c.onSurface),
-        ),
-        mainButton: TextButton(
-          child: const Text("Open Settings"),
-          onPressed: () {
-            openAppSettings();
-          },
         ),
       ));
     }
   }
-  // }
 }
